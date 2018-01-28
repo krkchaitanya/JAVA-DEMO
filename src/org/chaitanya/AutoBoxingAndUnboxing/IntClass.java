209507244b52a5bbbad5b76a7757723987b85ebb
krk chaitanya <krkchaitanya42@gmail.com>
2018-01-28 13:13:16 -0600

    AutoBoxingAndUnboxing

---
+++ src/org/chaitanya/AutoBoxingAndUnboxing/IntClass.java
@@ -0,0 +1,18 @@
+package org.chaitanya.AutoBoxingAndUnboxing;
+
+public class IntClass {
+
+    private int myValue;
+
+    public IntClass(int myValue){
+        this.myValue=myValue;
+    }
+
+    public int getMyValue() {
+        return myValue;
+    }
+
+    public void setMyValue(int myValue) {
+        this.myValue = myValue;
+    }
+}
--- src/org/chaitanya/Main.java
+++ src/org/chaitanya/Main.java
@@ -1,6 +1,7 @@
 package org.chaitanya;
 
 
+import org.chaitanya.AutoBoxingAndUnboxing.IntClass;
 import org.chaitanya.ListArray.GroceryList;
 import org.chaitanya.classesContsructors.Account;
 import org.chaitanya.classesContsructors.Car;
@@ -11,7 +12,11 @@ import org.chaitanya.inherit.Animal;
 import org.chaitanya.inherit.Dog;
 import org.chaitanya.polymorph.Movie;
 
+
+
 import java.util.Scanner;
+import java.util.ArrayList;
+
 
 
 public class Main {
@@ -29,24 +34,28 @@ public class Main {
         org.chaitanya.classesContsructors.Car porsche=new org.chaitanya.classesContsructors.Car();
         org.chaitanya.classesContsructors.Car holden=new Car();
         porsche.setModel("someModel");
-        System.out.println("model gonna be--"+porsche.getModel());
+//        System.out.println("model gonna be--"+porsche.getModel());
 
 
 //        Account class
         Account oneAcc=new Account("1234",0.00,"chaitanya","chai@email.com","12341231234");
 
         oneAcc.deposit(220.00);
-        System.out.println(oneAcc.getBalance());
+//        System.out.println(oneAcc.getBalance());
         oneAcc.withdraw(220.00);
-        System.out.println(oneAcc.getBalance());
+//        System.out.println(oneAcc.getBalance());
+
+
+
+
 
 //        Animal class
         Animal animal=new Animal("Animal",1,1,5,5);
 
         Dog dog=new Dog("Yorkie",8,20,2,4,1,4,20,20,"long silky");
 
-        System.out.println("dog coat--"+dog.getCoat());
-        System.out.println("animal name  "+animal.getName());
+//        System.out.println("dog coat--"+dog.getCoat());
+//        System.out.println("animal name  "+animal.getName());
 
         dog.chewing();
 
@@ -81,7 +90,7 @@ public class Main {
 
 
         EnhancedPlayer enhancedPlayer=new EnhancedPlayer("tim",200,"knife");
-        System.out.println("encapsulation.....gettingHealth---"+enhancedPlayer.getHealth());
+//        System.out.println("encapsulation.....gettingHealth---"+enhancedPlayer.getHealth());
 
 
 
@@ -140,57 +149,97 @@ public class Main {
             for (int i = 0; i < myIntegers.length; i++) {
                 System.out.println("Element " + i + "types was" + myIntegers[i]);
             }
-        System.out.println("average of array gonna be_______ "+getAvg(myIntegers));
+//        System.out.println("average of array gonna be_______ "+getAvg(myIntegers));
 
 
 
 //list array package/////////////////////////////////////////////
+//
+//
+//        GroceryList groceryList=new GroceryList();
+//
+//        boolean quit=false;
+//        int choice=0;
+//        printInstructions();
+//        while (!quit){
+//            System.out.println("---enter ur choice----");
+//            choice=scanner.nextInt();
+//            scanner.nextLine();
+//            switch(choice){
+//                case 0:{
+//                    printInstructions();
+//                    break;
+//                }
+//                case 1:{
+//                    groceryList.printItems();
+//                    break;
+//                }
+//                case 2:{
+//                    addItem();
+//                    break;
+//                }
+//                case 3:{
+//                    modifyItem();
+//                    break;
+//                }
+//                case 4:{
+//                    removeItem();
+//                    break;
+//                }
+//                case 5:{
+//                    searchFotItem();
+//                    break;
+//                }
+//                case 6:
+//                    quit=true;
+//                    break;
+//            }
+//        }
 
 
-        GroceryList groceryList=new GroceryList();
 
-        boolean quit=false;
-        int choice=0;
-        printInstructions();
-        while (!quit){
-            System.out.println("---enter ur choice----");
-            choice=scanner.nextInt();
-            scanner.nextLine();
-            switch(choice){
-                case 0:{
-                    printInstructions();
-                    break;
-                }
-                case 1:{
-                    groceryList.printItems();
-                    break;
-                }
-                case 2:{
-                    addItem();
-                    break;
-                }
-                case 3:{
-                    modifyItem();
-                    break;
-                }
-                case 4:{
-                    removeItem();
-                    break;
-                }
-                case 5:{
-                    searchFotItem();
-                    break;
-                }
-                case 6:
-                    quit=true;
-                    break;
-            }
+
+//--------------AUTOBOXING AND UNBOXING--------------
+
+        String[] strArray=new String[10];
+        int[] intArray=new int[10];
+
+        ArrayList<String> strArrayList=new ArrayList<String>();
+        strArrayList.add("tim bulchik");
+
+        ArrayList<IntClass> intClassArrayList=new ArrayList<IntClass>();
+        intClassArrayList.add(new IntClass(54));
+//        System.out.println("strArraylist----"+strArrayList);
+//        System.out.println("intClassArrayList======"+intClassArrayList.getClass());
+
+
+
+
+
+        ArrayList<Integer> intArrLi=new ArrayList<Integer>();
+        for (int i=0;i<=10;i++){
+            intArrLi.add(Integer.valueOf(i));
+        }
+
+        for (int i=0;i<intArrLi.size();i++){
+            System.out.println("auto boxing and unboxing......"+intArrLi.get(i).intValue());
+        }
+
+
+
+        ArrayList<Double> doubleArrLi=new ArrayList<Double>();
+        for (double i=0.0;i<=10.0;i+=0.5){
+            doubleArrLi.add(Double.valueOf(i));
+        }
+
+        for (int i=0;i<doubleArrLi.size();i++){
+            System.out.println("double array list------data---"+doubleArrLi.get(i).doubleValue());
         }
 
 
 
 
-
+//------------------------------
 
 
 
@@ -221,7 +270,12 @@ public class Main {
     }
 
 
-// print instructions--listArray package-----------------------
+
+/*
+---------------Methods-----------------Methods-----------Methods--------------------------------------------------------------------------------------
+................................................................................................................................................
+print instructions--listArray package-----------------------
+*/
 
     public static void printInstructions(){
         System.out.println("0--choose some no");
@@ -262,8 +316,6 @@ public class Main {
             System.out.println(ite+"item not found--");
         }
     }
-
-
 
 
 
