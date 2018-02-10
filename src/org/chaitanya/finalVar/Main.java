6f9a2f2c7ad888547ade6195465464b4969730c3
krk chaitanya <krkchaitanya42@gmail.com>
2018-02-10 02:02:05 -0600

    finalVar

---
+++ src/org/chaitanya/finalVar/Main.java
@@ -0,0 +1,14 @@
+package org.chaitanya.finalVar;
+
+public class Main {
+
+    public static void main(String[] args){
+
+
+       SomeClass one=new SomeClass("one");
+       SomeClass two=new SomeClass("two");
+       SomeClass three=new SomeClass("three");
+
+    }
+
+}
