7b1542730569e3ea195a4b1b0e7f00c89e14a628
krk chaitanya <krkchaitanya42@gmail.com>
2018-03-24 09:48:21 -0500

    studentSearchPolymorph

--- src/org/chaitanya/jdbc/Database.java
+++ src/org/chaitanya/jdbc/Database.java
@@ -49,7 +49,46 @@ public class Database {
             con.close();
 
         }catch(Exception e){ System.out.println(e);}
-        }
+        };
+
+
+    public void searchStudent(String name){
+        try{
+//step1 load the driver class
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+
+//step2 create  the connection object
+            Connection con=DriverManager.getConnection(
+                    "jdbc:oracle:thin:@localhost:1521:xe","system","password");
+
+//step3 create the statement object
+            Statement stmt=con.createStatement();
+
+
+            ResultSet rs=stmt.executeQuery(
+                    "SELECT * from student where student_name='"+name+"'");
+
+            while(rs.next()) {
+                stu.setStudent_id(rs.getInt(1));
+                stu.setStudent_name(rs.getString(2));
+                stu.setStudent_age(rs.getInt(3));
+                stu.setStudent_dob(rs.getString(4));
+
+                System.out.println("student data................");
+                System.out.println("ID :" + stu.getStudent_id());
+                System.out.println("NAME :" + stu.getStudent_name());
+                System.out.println("AGE :" + stu.getStudent_age());
+                System.out.println("DOB :" + stu.getStudent_dob());
+            }
+
+//                System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+
+//                        rs.getInt(3)+" "+rs.getString(4));
+
+//step5 close the connection object
+            con.close();
+
+        }catch(Exception e){ System.out.println(e);}
+    }
 
 
 }
--- src/org/chaitanya/jdbc/Main.java
+++ src/org/chaitanya/jdbc/Main.java
@@ -7,7 +7,14 @@ public class Main {
         Database db=new Database();
         Scanner scanner=new Scanner(System.in);
         System.out.println("Select options ...11..12..13..14.....");
+        System.out.println("Enter student_id");
         int findVal=scanner.nextInt();
         db.searchStudent(findVal);
+        System.out.println("==============================");
+        System.out.println("select options...sandeep...venkat...shrawan....chaitanya..........");
+        System.out.println("enter student_name");
+        String person=scanner.nextLine();
+        db.searchStudent(person);
+
     }
 }
