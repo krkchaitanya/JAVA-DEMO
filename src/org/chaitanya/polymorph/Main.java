588625850cf9695e6dd17ba76f240338f8846706
krk chaitanya <krkchaitanya42@gmail.com>
2018-04-25 23:37:08 -0500

    mainUpdatePolymorphism

--- src/org/chaitanya/polymorph/Main.java
+++ src/org/chaitanya/polymorph/Main.java
@@ -1,7 +1,38 @@
 package org.chaitanya.polymorph;
 
 public class Main {
-    public static void main(String[] args){
-        
+    public static void main(String[] args) {
+
+
+        MazeRunner mazeRunner = new MazeRunner();
+        System.out.println(mazeRunner.plot());
+        System.out.println((int) (Math.random() * 5));
+
+        for (int i=0;i<5;i++){
+            int randomNum=(int)(Math.random()*4);
+            Movie movie=movieHandler(randomNum);
+            System.out.println(movie.plot());
+        }
+
+    }
+        public static Movie movieHandler(int randomNum){
+
+        switch(randomNum){
+            case 1:
+                return new Jaws();
+
+            case 2:
+                return new StarWars();
+
+            case 3:
+                return new IndependenceDay();
+
+            case 4:
+                return new Forgettable();
+
+                default:
+                return null;
+        }
+
     }
 }
